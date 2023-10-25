d5e25c2f1fbe94b12398bec3c4e0ccef2ef081cf
Stephanie Cure Chegwin <stephanie.cure@willowtreeapps.com>
2023-10-25 13:47:25 -0700

    Add date picker

--- App.tsx
+++ App.tsx
@@ -1,17 +1,45 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import HealthData from "./components/HealthData";
 import RingProgress from "./components/RingProgress";
 import useHealthData from "./hooks/useHealthData";
+import { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
 
 const STEPS_GOAL = 10000;
 
 export default function App() {
-  const { steps, distance, flights } = useHealthData();
+  const [date, setDate] = useState(new Date());
+  const { steps, distance, flights } = useHealthData(date);
+
+  const changeDate = (numDays) => {
+    const currentDate = new Date(date); // Create a copy of the current date
+    // Update the date by adding/subtracting the number of days
+    currentDate.setDate(currentDate.getDate() + numDays);
+
+    setDate(currentDate); // Update the state variable
+  };
 
   return (
     <View style={styles.container}>
-      <RingProgress progress={6000 / STEPS_GOAL} />
+      <View style={styles.datePicker}>
+        <AntDesign
+          onPress={() => changeDate(-1)}
+          name="left"
+          size={20}
+          color="#C3FF53"
+        />
+        <Text style={styles.date}>{date.toDateString()}</Text>
+
+        <AntDesign
+          onPress={() => changeDate(1)}
+          name="right"
+          size={20}
+          color="#C3FF53"
+        />
+      </View>
+
+      <RingProgress progress={steps / STEPS_GOAL} />
 
       <View style={styles.dataContainer}>
         <HealthData label="Steps" value={steps.toString()} />
@@ -39,5 +67,17 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     marginVertical: 20,
+  },
+  datePicker: {
+    alignItems: "center",
+    padding: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  date: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 20,
+    marginHorizontal: 20,
   },
 });
--- hooks/useHealthData.tsx
+++ hooks/useHealthData.tsx
@@ -16,7 +16,7 @@ const permissions: HealthKitPermissions = {
   },
 };
 
-const useHealthData = () => {
+const useHealthData = (date: Date) => {
   const [hasPermissions, setHasPermission] = useState(false);
   const [steps, setSteps] = useState(0);
   const [flights, setFlights] = useState(0);
@@ -55,7 +55,7 @@ const useHealthData = () => {
     }
 
     const options: HealthInputOptions = {
-      date: new Date().toISOString(),
+      date: date.toISOString(),
       includeManuallyAdded: false,
     };
 
@@ -85,60 +85,10 @@ const useHealthData = () => {
       }
       setDistance(results.value);
     });
-  }, [hasPermissions]);
-
-  //   // Android - Health Connect
-  //   const readSampleData = async () => {
-  //     // initialize the client
-  //     const isInitialized = await initialize();
-  //     if (!isInitialized) {
-  //       return;
-  //     }
-
-  //     // request permissions
-  //     await requestPermission([
-  //       { accessType: "read", recordType: "Steps" },
-  //       { accessType: "read", recordType: "Distance" },
-  //       { accessType: "read", recordType: "FloorsClimbed" },
-  //     ]);
-
-  //     const timeRangeFilter: TimeRangeFilter = {
-  //       operator: "between",
-  //       startTime: new Date(date.setHours(0, 0, 0, 0)).toISOString(),
-  //       endTime: new Date(date.setHours(23, 59, 59, 999)).toISOString(),
-  //     };
-
-  //     // Steps
-  //     const steps = await readRecords("Steps", { timeRangeFilter });
-  //     const totalSteps = steps.reduce((sum, cur) => sum + cur.count, 0);
-  //     setSteps(totalSteps);
-
-  //     // Distance
-  //     const distance = await readRecords("Distance", { timeRangeFilter });
-  //     const totalDistance = distance.reduce(
-  //       (sum, cur) => sum + cur.distance.inMeters,
-  //       0
-  //     );
-  //     setDistance(totalDistance);
-
-  //     // Floors climbed
-  //     const floorsClimbed = await readRecords("FloorsClimbed", {
-  //       timeRangeFilter,
-  //     });
-  //     const totalFloors = floorsClimbed.reduce((sum, cur) => sum + cur.floors, 0);
-  //     setFlights(totalFloors);
-  //     // console.log(floorsClimbed);
-  //   };
-
-  //   useEffect(() => {
-  //     if (Platform.OS !== "android") {
-  //       return;
-  //     }
-  //     readSampleData();
-  //   }, [date]);
+  }, [hasPermissions, date]);
 
   return {
-    steps,
+    steps: steps.toFixed(0),
     flights,
     distance,
   };
